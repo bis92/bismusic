8d1fa8420076e82025962f0b8af6002867b1ef01
Inseob Byun <bis920309@mail.google.com>
2021-06-07 02:27:14 +0900

    프론트 단 js 파일 만들기

--- app/app.js
+++ app/app.js
@@ -2,6 +2,8 @@
 // 모듈
 import express from 'express'
 const app = express();
+import path from 'path';
+const __dirname = path.resolve();
 
 // 라우팅
 import home from './src/routes/home/index.js';
@@ -9,6 +11,7 @@ import home from './src/routes/home/index.js';
 // 앱 세팅
 app.set("views", "./src/views");
 app.set("view engine", "ejs");
+app.use(express.static(`${__dirname}/src/public`));
 
 app.use("/", home);
 
