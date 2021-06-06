5fdfdf0d026604175debb042049f00af3c44b9a1
Inseob Byun <bis920309@mail.google.com>
2021-06-07 02:02:26 +0900

    폴더 구조 최적화

--- app/app.js
+++ app/app.js
@@ -4,10 +4,10 @@ import express from 'express'
 const app = express();
 
 // 라우팅
-import home from './routes/home/index.js';
+import home from './src/routes/home/index.js';
 
 // 앱 세팅
-app.set("views", "./views");
+app.set("views", "./src/views");
 app.set("view engine", "ejs");
 
 app.use("/", home);
