5419978a3c4642e2157c0e109c2ea66491ada301
Inseob Byun <bis920309@mail.google.com>
2021-06-07 19:50:26 +0900

    회원가입 라우팅 구현

--- app/src/public/js/home/register.js
+++ app/src/public/js/home/register.js
@@ -9,11 +9,13 @@ registerBtn = document.querySelector("#button");
 registerBtn.addEventListener("click", register);
 
 function register() {
+    if(!id.value) return alert("아이디를 입력해주세요!");
+    if(password !== confirmPassword) return alert("비밀번호가 일치하지 않습니다!");
+    
     const req = {
         id: id.value,
         name: name.value,
         password: password.value,
-        confirmPassword: confirmPassword.value
     };
     console.log(req);
 
